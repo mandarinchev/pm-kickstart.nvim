a509d8e959c9c32c0fc8a8f804bef9e0de206135
Plamen Marinchev <marinchevp@gmail.com>
2024-12-15 21:10:47 +0200

    New colorscheme to rule them.

--- init.lua
+++ init.lua
@@ -749,7 +749,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'habamax'
+      vim.cmd.colorscheme 'lunaperche'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
